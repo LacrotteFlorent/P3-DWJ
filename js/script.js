867e9ede102fb520c444f9482b338ff441f497fa
Lacrotte Florent <lacrotte.florent@gmail.com>
2019-11-04 09:52:36 +0100

    premier pas en POO

--- js/script.js
+++ js/script.js
@@ -83,6 +83,7 @@ function prevDiapo () {
 // Délai de 5 sec entre chaque diapo
 let intervalID;
 let tempoOn = true; // Vrai si la diapo est en mode lecture
+$('.fa-play').css('color','#7A050C'); //
 function tempo() {
     intervalID = setInterval(nextDiapo,5000);
 }
@@ -91,8 +92,8 @@ function tempo() {
 $(function() {
     tempo();
     $('#btnPrev').on('click', function() {prevDiapo(); console.log("clic precedent");});
-    $('#btnPlay').on('click', function() {tempo(); console.log("clic play"); tempoOn=true;});
-    $('#btnPause').on('click', function() {clearInterval(intervalID); tempoOn=false; console.log("clic pause");});
+    $('#btnPlay').on('click', function() {tempo(); console.log("clic play"); tempoOn=true;$('.fa-play').css('color','#7A050C');$('.fa-pause').css('color','#F7BE03');});
+    $('#btnPause').on('click', function() {clearInterval(intervalID); tempoOn=false; console.log("clic pause");$('.fa-play').css('color','#F7BE03');$('.fa-pause').css('color','#7A050C');});
     $('#btnNext').on('click', function () {nextDiapo(); console.log("clic suivant");}); 
 });
 
@@ -130,8 +131,9 @@ function imageSlide() {
     }
 }
 
-                                                                            /* CARTE */
 
+                                                                            /* CARTE */
+/*
     let intervalActualisation;
     function tempoRefresh() {
         refresh();
@@ -149,8 +151,6 @@ function imageSlide() {
         attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
         maxZoom: 18,
         minZoom: 13,
-        /*id: 'mapbox.streets',
-        accessToken: 'your.mapbox.access.token'*/
     }).addTo(macarte);
 
     let marqueurs = L.markerClusterGroup({disableClusteringAtZoom: 16});
@@ -162,25 +162,25 @@ function imageSlide() {
 
             // Personnalisation des marqueurs
             let iconeOpen = L.icon({
-                iconUrl: "img/icon_marqueurs_open.png",
+                iconUrl: "img/icon_marqueurs_open_2.png",
                 iconSize: [40, 40],
                 icon: [25, 50],
                 popupAnchor: [0, -15]
             })
             let iconeClose = L.icon({
-                iconUrl: "img/icon_marqueurs_close.png",
+                iconUrl: "img/icon_marqueurs_close_2.png",
                 iconSize: [40, 40],
                 icon: [25, 50],
                 popupAnchor: [0, -15]
             })
             let iconeOnlyBike = L.icon({
-                iconUrl: "img/icon_marqueurs_bike.png",
+                iconUrl: "img/icon_marqueurs_bike_2.png",
                 iconSize: [40, 40],
                 icon: [25, 50],
                 popupAnchor: [0, -15]
             })
             let iconeOnlyStand = L.icon({
-                iconUrl: "img/icon_marqueurs_stand.png",
+                iconUrl: "img/icon_marqueurs_stand_2.png",
                 iconSize: [40, 40],
                 icon: [25, 50],
                 popupAnchor: [0, -15]
@@ -188,9 +188,11 @@ function imageSlide() {
             
             macarte.removeLayer(marqueurs); // On suprimme les précédents marqueurs avant d'actualiser par les nouveaux
             // On ajoute les marqueurs par station
+            let marqueur;
+            marqueurs.clearLayers();
             for (station of stations) {
                // Nous ajoutons les marqueurs en fonctions de leurs status
-               let marqueur = "";
+               
                 if((station.status === "OPEN") && (station.totalStands.availabilities.bikes >= 1) && (station.totalStands.availabilities.stands >= 1)) {
                     marqueur = L.marker([station.position.latitude, station.position.longitude], {icon: iconeOpen})//.addTo(macarte); inutiles lors de l'utilisation des clusters
                 }
@@ -220,6 +222,7 @@ function imageSlide() {
                     // On ajoute la popup au marqueur
                     marqueur.bindPopup(htmlPopup, optionsPopup);
                     marqueurs.addLayer(marqueur); // On ajoute le marqueur au layer qui correspond au cluster
+                    
                 }
                 else {
                     // On défini le texte de la popup
@@ -234,6 +237,7 @@ function imageSlide() {
                     // On ajoute la popup au marqueur
                     marqueur.bindPopup(htmlPopup, optionsPopup);
                     marqueurs.addLayer(marqueur); // On ajoute le marqueur au layer qui correspond au cluster
+                
                 }
             }
             macarte.addLayer(marqueurs);
@@ -241,23 +245,153 @@ function imageSlide() {
     };
 
 
+    $('body').on('click', '.btnSub', function() {
+        console.log('click');
+        //$("#informations").removeClass("d-none").addClass("d-block col-lg-3");
+        //$("#carte").removeClass("col-lg-12").addClass("col-lg-9");
+    });
 
-
-
+*/
 /////////////////////////////////////////////////CODE OBJET///////////////////////////////////////////
 
+let intervalActualisation;
+    function tempoRefresh() {
+        refresh();
+        intervalActualisation = setInterval(refresh,60000);
+    }
+
+    //tempoRefresh();
+    // initialisation de la carte
+    macarte = L.map('carte').setView([-27.482279, 153.028723], 13);
+            
+    // Chargement des tuiles
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+        attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+        maxZoom: 18,
+        minZoom: 13,
+    }).addTo(macarte);
+
+    let marqueurs = L.markerClusterGroup({disableClusteringAtZoom: 16});
+
+    class Station {
+
+        marqueur;
+
+        constructor(adresse, status, nbVelo, nbPlace, latitude, longitude) {
+            this.adresse = adresse;
+            this.status = status;
+            this.nbVelo = nbVelo;
+            this.nbPlace = nbPlace;
+            this.latitude = latitude;
+            this.longitude = longitude;
+        }
+
+        afficheDetail () {
+            console.log(this.adresse);
+            console.log(this.status);
+            console.log(this.nbVelo);
+            console.log(this.nbPlace);
+            console.log(this.longitude);
+            console.log(this.latitude);
+        }
+    
+        genererMarqueur () {
+            // On change l'icone du marqueur en fonction des données de la station
+            let iconeOpen = L.icon({
+                iconUrl: "img/icon_marqueurs_open_2.png",
+                iconSize: [40, 40],
+                icon: [25, 50],
+                popupAnchor: [0, -15]
+            })
+            let iconeClose = L.icon({
+                iconUrl: "img/icon_marqueurs_close_2.png",
+                iconSize: [40, 40],
+                icon: [25, 50],
+                popupAnchor: [0, -15]
+            })
+            let iconeOnlyBike = L.icon({
+                iconUrl: "img/icon_marqueurs_bike_2.png",
+                iconSize: [40, 40],
+                icon: [25, 50],
+                popupAnchor: [0, -15]
+            })
+            let iconeOnlyStand = L.icon({
+                iconUrl: "img/icon_marqueurs_stand_2.png",
+                iconSize: [40, 40],
+                icon: [25, 50],
+                popupAnchor: [0, -15]
+            })
+
+            if((this.status === "OPEN") && (this.nbVelo >= 1) && (this.nbPlace >= 1)) {
+                this.marqueur = L.marker([this.latitude, this.longitude], {icon: iconeOpen})
+            }
+            else if((this.status === "OPEN") && (this.nbVelo >= 1) && (this.nbPlace <= 0)) {
+                this.marqueur = L.marker([this.latitude, this.longitude], {icon: iconeOnlyBike})
+            }
+            else if((this.status === "OPEN") && (this.nbVelo <= 0) && (this.nbPlace >= 1)) {
+                this.marqueur = L.marker([this.latitude, this.longitude], {icon: iconeOnlyStand})
+            }
+            else {
+                this.marqueur = L.marker([this.latitude, this.longitude], {icon: iconeClose})
+            }
+        }
+    
+        genererPopup () {
+            // On affiche les popup en fonction du status de la station
+            if (this.status === "OPEN") {   // Si la station à le status ouvert
+                // On défini le texte de la popup
+                let htmlPopup = (
+                    "<p> <span class=mentions>Adresse:</span> " + this.adresse + "<p>" + 
+                    "<p> <span class=mentions>Vélos disponibles:</span> " + this.nbVelo + "<p>" +
+                    "<p> <span class=mentions>Places libres:</span> " + this.nbPlace + "<p>" +
+                    "<button class=btnSub> Réserver </button>"
+                );
+                
+                // On défini la classe de la popup
+                let optionsPopup = {'className' : 'stOpen'};
+                
+                // On ajoute la popup au marqueur
+                this.marqueur.bindPopup(htmlPopup, optionsPopup);
+            }
+            else {                          // Si la station à le status fermé
+                // On défini le texte de la popup
+                let htmlPopup = (
+                    "<p> <span class=mentions>Station fermée</span> <p>" + 
+                    "<p> <span class=mentions>Adresse:</span> " + station.adresse + "<p>"
+                );
+    
+                // On défini la classe de la popup
+                let optionsPopup = {'className' : 'stClose'};
+    
+                // On ajoute la popup au marqueur
+                this.marqueur.bindPopup(htmlPopup, optionsPopup);
+            }
+        }
+    
+    }
 
 
+function refresh () {
+    $.getJSON("https://api.jcdecaux.com/vls/v3/stations?contract=brisbane&apiKey=" + apiKey, function(stations, status) {
+
+        console.log("Status de l'appel JCDecaux : " + status);    
+
+        marqueurs.clearLayers();
+        
+        for (station of stations) {
+            const stationVelilov = new Station(station.address, station.status, station.totalStands.availabilities.bikes, station.totalStands.availabilities.stands, station.position.latitude, station.position.longitude);
+            stationVelilov.genererMarqueur();
+            //stationVelilov.afficheDetail();
+            stationVelilov.genererPopup();
+            marqueurs.addLayer(stationVelilov.marqueur);
+        }
+        macarte.addLayer(marqueurs);
+
+    });
+};
 
 
-
-
-
-
-
-
-
-
+refresh();
 
 
 
